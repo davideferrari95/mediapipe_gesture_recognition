a5024a8ad48e65170ea760f1b64ab1c3f111ab11
Tanguy Sorel <tanguy.sorel@2022.icam.fr>
2022-03-24 12:20:09 +0100

    some mod

--- scripts/mediapipe_stream_node.py
+++ scripts/mediapipe_stream_node.py
@@ -2,7 +2,7 @@
 
 import rospy
 import mediapipe as mp
-from mediapipe_gesture_recognition.msg import Hand, Skeleton, Face
+from mediapipe_gesture_recognition.msg import Hand, Skeleton, Face, Keypoint
 
 
 rospy.init_node('mediapipe_stream_node', anonymous=True)
@@ -17,6 +17,34 @@ hand_right_msg = Hand()
 hand_left_msg = Hand()
 skeleton_msg = Skeleton()
 
-# Run mediapipe detection
-# add keypoint to each message ordered
-# publish each message
+# initialization
+right_hand = False
+
+
+
+while not rospy.is_shutdown():
+
+    if right_hand:
+
+        # Run mediapipe right_hand detection
+
+        hand_right_msg.right_or_left = hand_right_msg.RIGHT
+
+        for i in range(21):
+            # Read keypoint
+            new_keypoint = Keypoint()
+            new_keypoint.x = ...
+            new_keypoint.y = ...
+            new_keypoint.z = ...
+            new_keypoint.v = ...
+            new_keypoint.keypoint_number
+            new_keypoint.keypoint_name
+            # Append keypoint
+            hand_right_msg.keypoints.append(new_keypoint)
+
+        hand_right_pub(hand_right_msg)
+
+    # Run mediapipe detection
+    # add keypoint to each message ordered
+    # publish each message
+
