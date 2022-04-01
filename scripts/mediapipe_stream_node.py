28fa90efd04b69f804595b223d9ccf54a44de981
Tanguy Sorel <tanguy.sorel@2022.icam.fr>
2022-04-01 15:12:11 +0200

    Correct detection of landmarks with holistic

--- scripts/mediapipe_stream_node.py
+++ scripts/mediapipe_stream_node.py
@@ -25,7 +25,6 @@ face_msg = Face()
 webcam = rospy.get_param('webcam', 0)
 
 # Read Mediapipe Modules Parameters
-enable_holistic = rospy.get_param('enable_holistic', True)
 enable_right_hand = rospy.get_param('enable_right_hand', False)
 enable_left_hand = rospy.get_param('enable_left_hand', False)
 enable_pose = rospy.get_param('enable_pose', False)
@@ -52,7 +51,7 @@ cap = cv2.VideoCapture(webcam)
 # While loop mediapipe code that need to be run each cycle
 while not rospy.is_shutdown():
 
-  with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands, mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose, mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh, mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+  with mp_holistic.Holistic(refine_face_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
       success, image = cap.read()
       if not success:
@@ -63,11 +62,15 @@ while not rospy.is_shutdown():
       # To improve performance, optionally mark the image as not writeable to pass by reference.
       image.flags.writeable = False
       image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+        
+      # Draw the annotations on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
       if enable_right_hand or enable_left_hand:
         hand_results = holistic.process(image)
 
+        #Drawing the right hand landmarks
         if enable_right_hand:
           mp_drawing.draw_landmarks(
               image,
@@ -76,43 +79,44 @@ while not rospy.is_shutdown():
               mp_drawing_styles.get_default_hand_landmarks_style(),
               mp_drawing_styles.get_default_hand_connections_style())
 
-          #add right_hand keypoint to ordered message
-          hand_right_msg.right_or_left = hand_right_msg.RIGHT
+          if hand_results.right_hand_landmarks:
+            #add right_hand keypoint to ordered message
+            hand_right_msg.right_or_left = hand_right_msg.RIGHT
 
-          # Run mediapipe right_hand detection (before and obtain the landmarks)
-          for i in range(len(hand_results.right_hand_landmarks.landmark)):
-          
-            """
-            landmark {
-              x: 0.414364755153656
-              y: 0.2742988169193268
-              z: -0.5210666060447693
-              visibility: 0.9999992847442627
-            }
-            landmark {
-              x: 0.4284072518348694
-              y: 0.2577638030052185
-              z: -0.49063441157341003
-              visibility: 0.9999985694885254
-            } 
-            """
+            for i in range(len(hand_results.right_hand_landmarks.landmark)):
+            
+              """
+              landmark {
+                x: 0.414364755153656
+                y: 0.2742988169193268
+                z: -0.5210666060447693
+                visibility: 0.9999992847442627
+              }
+              landmark {
+                x: 0.4284072518348694
+                y: 0.2577638030052185
+                z: -0.49063441157341003
+                visibility: 0.9999985694885254
+              } 
+              """
 
-            # Read keypoint
-            new_keypoint = Keypoint()
-            new_keypoint.x = hand_results.right_hand_landmarks.landmark[i].x
-            new_keypoint.y = hand_results.right_hand_landmarks.landmark[i].y
-            new_keypoint.z = hand_results.right_hand_landmarks.landmark[i].z
-            new_keypoint.v = hand_results.right_hand_landmarks.landmark[i].visibility
+              # Read keypoint
+              new_keypoint = Keypoint()
+              new_keypoint.x = hand_results.right_hand_landmarks.landmark[i].x
+              new_keypoint.y = hand_results.right_hand_landmarks.landmark[i].y
+              new_keypoint.z = hand_results.right_hand_landmarks.landmark[i].z
+              new_keypoint.v = hand_results.right_hand_landmarks.landmark[i].visibility
 
-            new_keypoint.keypoint_number = i
-            new_keypoint.keypoint_name = hand_landmarks_names[i]
+              new_keypoint.keypoint_number = i
+              new_keypoint.keypoint_name = hand_landmarks_names[i]
 
-            # Append keypoint
-            hand_right_msg.keypoints.append(new_keypoint)
-          
-          hand_right_pub.publish(hand_right_msg)
+              # Append keypoint
+              hand_right_msg.keypoints.append(new_keypoint)
 
-        #add left_hand keypoint to ordered message
+            hand_right_pub.publish(hand_right_msg)
+
+
+        #Drawing the left hand landmarks
         if enable_left_hand:
           mp_drawing.draw_landmarks(
               image,
@@ -122,126 +126,125 @@ while not rospy.is_shutdown():
               mp_drawing_styles.get_default_hand_connections_style())
 
           #add left_hand keypoint to ordered message
-          hand_left_msg.right_or_left = hand_left_msg.LEFT
-
-          # Run mediapipe left_hand detection
-          for i in range(len(hand_results.left_hand_landmarks.landmark)):
-          
-            # Read keypoint
-            new_keypoint = Keypoint()
-            new_keypoint.x = hand_results.left_hand_landmarks.landmark[i].x
-            new_keypoint.y = hand_results.left_hand_landmarks.landmark[i].y
-            new_keypoint.z = hand_results.left_hand_landmarks.landmark[i].z
-            new_keypoint.v = hand_results.left_hand_landmarks.landmark[i].visibility
-
-            new_keypoint.keypoint_number = i
-            new_keypoint.keypoint_name = hand_landmarks_names[i]
-
-            # Append keypoint
-            hand_left_msg.keypoints.append(new_keypoint)
-
-          hand_left_pub.publish(hand_left_msg)
+          if hand_results.left_hand_landmarks:
+            #add left_hand keypoint to ordered message
+            hand_left_msg.right_or_left = hand_left_msg.RIGHT
+  
+            # Run mediapipe right_hand detection (before and obtain the landmarks)
+            for i in range(len(hand_results.left_hand_landmarks.landmark)):
+            
+              """
+              landmark {
+                x: 0.414364755153656
+                y: 0.2742988169193268
+                z: -0.5210666060447693
+                visibility: 0.9999992847442627
+              }
+              landmark {
+                x: 0.4284072518348694
+                y: 0.2577638030052185
+                z: -0.49063441157341003
+                visibility: 0.9999985694885254
+              } 
+              """
+  
+              # Read keypoint
+              new_keypoint = Keypoint()
+              new_keypoint.x = hand_results.left_hand_landmarks.landmark[i].x
+              new_keypoint.y = hand_results.left_hand_landmarks.landmark[i].y
+              new_keypoint.z = hand_results.left_hand_landmarks.landmark[i].z
+              new_keypoint.v = hand_results.left_hand_landmarks.landmark[i].visibility
+  
+              new_keypoint.keypoint_number = i
+              new_keypoint.keypoint_name = hand_landmarks_names[i]
+  
+              # Append keypoint
+              hand_left_msg.keypoints.append(new_keypoint)
+            
+            hand_left_pub.publish(hand_left_msg)
 
 
       if enable_pose:
-        pose_results = pose.process(image)
+        pose_results = holistic.process(image)
 
-        # Draw the pose annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        #Drawing the pose landmarks
         mp_drawing.draw_landmarks(
             image,
             pose_results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())    
+            mp_holistic.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
-        #add pose keypoint to ordered message   
-        for i in range(len(pose_results.pose_landmarks.landmarks)): # BUG : TypeError: object of type 'NoneType' has no len()
+        if pose_results.pose_landmarks:
 
-          """
-          x: 0.6068623065948486
-          y: 0.6184841394424438
-          z: -0.22558169066905975
-          visibility: 0.9960732460021973
-          , x: -0.21233102679252625
-          y: 0.6388503909111023
-          z: 0.1573469489812851
-          visibility: 0.9742392301559448
-          """
+          #add pose keypoint to ordered message   
+          for i in range(len(pose_results.pose_landmarks.landmark)): # BUG : TypeError: object of type 'NoneType' has no len()
 
-          # Read keypoint
-          new_keypoint = Keypoint()
-          new_keypoint.x = pose_results.pose_landmarks[i].landmark[i].x
-          new_keypoint.y = pose_results.pose_landmarks[i].landmark[i].y
-          new_keypoint.z = pose_results.pose_landmarks[i].landmark[i].z
-          new_keypoint.v = pose_results.pose_landmarks[i].landmark[i].visibility
-          new_keypoint.keypoint_number = i
-          new_keypoint.keypoint_name = pose_landmarks_names[i]
-          # Append keypoint
-          pose_msg.keypoints.append(new_keypoint)
+            """
+            x: 0.6068623065948486
+            y: 0.6184841394424438
+            z: -0.22558169066905975
+            visibility: 0.9960732460021973
+            , x: -0.21233102679252625
+            y: 0.6388503909111023
+            z: 0.1573469489812851
+            visibility: 0.9742392301559448
+            """
 
-        pose_pub.publish(pose_msg)    
+            # Read keypoint
+            new_keypoint = Keypoint()
+            new_keypoint.x = pose_results.pose_landmarks.landmark[i].x
+            new_keypoint.y = pose_results.pose_landmarks.landmark[i].y
+            new_keypoint.z = pose_results.pose_landmarks.landmark[i].z
+            new_keypoint.v = pose_results.pose_landmarks.landmark[i].visibility
+            new_keypoint.keypoint_number = i
+            new_keypoint.keypoint_name = pose_landmarks_names[i]
+            # Append keypoint
+            pose_msg.keypoints.append(new_keypoint)
+
+          pose_pub.publish(pose_msg)    
 
 
       if enable_face:
-        face_results = face_mesh.process(image)
+        face_results = holistic.process(image)
 
-        # Draw the face detection annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if face_results.multi_face_landmarks:
-              for face_landmarks in face_results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_tesselation_style())
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_contours_style())
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_IRISES,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_iris_connections_style())
+        #Drawing the face landmarks
+        mp_drawing.draw_landmarks(
+            image,
+            face_results.face_landmarks,
+            mp_holistic.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
 
-        #add face keypoint to ordered message
-        for i in range(len(face_results.multi_face_landmarks.landmarks)): # BUG : TypeError: object of type 'NoneType' has no len()
-          """
-          landmark {
-            x: 0.6572769284248352
-            y: 0.44084274768829346
-            z: -0.02765425108373165
-          }
-          landmark {
-            x: 0.6293548941612244
-            y: 0.4240654706954956
-            z: -0.02765425108373165
-          }
-          """
-
-          # Read keypoint
-          new_keypoint = Keypoint()
-          new_keypoint.x = face_results.multi_face_landmarks[i].landmark[i].x
-          new_keypoint.y = face_results.multi_face_landmarks[i].landmark[i].y
-          new_keypoint.z = face_results.multi_face_landmarks[i].landmark[i].z
-          # BUG: new_keypoint.v = pose_results.face_landmarks.landmark[i].visibility
-          new_keypoint.keypoint_number = i
-
-          # 468 Landmarks so custom names as FACE_KEYPOINT_1 ...
-          new_keypoint.keypoint_name = f'FACE_KEYPOINT_{i}'
-          # Append keypoint
-          face_msg.keypoints.append(new_keypoint)
-
-        face_pub.publish(face_msg) 
+        if face_results.face_landmarks:
+          #add face keypoint to ordered message
+          for i in range(len(face_results.face_landmarks.landmark)): # BUG : TypeError: object of type 'NoneType' has no len()
+            """
+            landmark {
+              x: 0.6572769284248352
+              y: 0.44084274768829346
+              z: -0.02765425108373165
+            }
+            landmark {
+              x: 0.6293548941612244
+              y: 0.4240654706954956
+              z: -0.02765425108373165
+            }
+            """
+  
+            # Read keypoint
+            new_keypoint = Keypoint()
+            new_keypoint.x = face_results.face_landmarks.landmark[i].x
+            new_keypoint.y = face_results.face_landmarks.landmark[i].y
+            new_keypoint.z = face_results.face_landmarks.landmark[i].z
+            # BUG: new_keypoint.v = pose_results.face_landmarks.landmark.visibility
+            new_keypoint.keypoint_number = i
+  
+            # 468 Landmarks so custom names as FACE_KEYPOINT_1 ...
+            new_keypoint.keypoint_name = f'FACE_KEYPOINT_{i}'
+            # Append keypoint
+            face_msg.keypoints.append(new_keypoint)
+  
+          face_pub.publish(face_msg) 
 
 
       # Flip the image horizontally for a selfie-view display.
