8582301f1371a269839f5bcc07114ff056574136
davideferrari95 <davideferr@unimore.it>
2024-04-22 17:19:26 +0200

    Moved Video Register

--- scripts/dataset_utilities/video_register.py
+++ scripts/dataset_utilities/video_register.py
@@ -1,7 +1,33 @@
 import os
 import cv2
 import time, rospy
-from Utils import Start_countdown 
+
+# Countdown Function
+def start_countdown(num_of_secs):
+
+    print("\nAcquisition Starts in:")
+
+    # Wait Until 0 Seconds Remaining
+    while (not rospy.is_shutdown() and num_of_secs != 0):
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        print(min_sec_format)
+        rospy.sleep(1)
+        num_of_secs -= 1
+
+    print("\nSTART\n")
+
+def rest_countdown(num_of_secs):
+
+
+    # Wait Until 0 Seconds Remaining
+    while (not rospy.is_shutdown() and num_of_secs != 0):
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        print(min_sec_format)
+        rospy.sleep(1)
+        num_of_secs -= 1
+
 
 # Input del numero di video da registrare
 num_videos = int(input("Inserisci il numero di video che vuoi registrare: "))
