0afaa4de8f7b1210dad7be66e5064790f147a840
davideferrari95 <davideferr@unimore.it>
2024-04-26 11:47:01 +0200

    Re-Written Point-At Nodes -> Get Area Coordinates
    !! TO TEST !!

--- scripts/raw_function/3Ditem_register.py
+++ scripts/raw_function/3Ditem_register.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python3
 
-import rospy, numpy as np
+import rospy, csv, numpy as np
+from typing import List, Tuple
 from mediapipe_gesture_recognition.msg import Hand, Pose
-from Utils import Start_countdown
-import csv
 
-class ItemRegister:
+class CoordinateRegister:
+
+    """ Coordinate Register Class for Point-At Gesture """
 
     def __init__(self):
 
-        # Inizializzazione del nodo
-        rospy.init_node('pointer_subscriber', anonymous=True)
+        # Node Initialization
+        rospy.init_node('item_register', anonymous=True)
 
-        # Inizializzazione dei topic
-        rospy.Subscriber("/mediapipe_gesture_recognition/right_hand", Hand, self.RightHandCallback)
-        rospy.Subscriber("/mediapipe_gesture_recognition/left_hand", Hand, self.LeftHandCallback)
-        rospy.Subscriber("/mediapipe_gesture_recognition/pose", Pose, self.PoseCallback)
+        # Subscribers Initialization
+        rospy.Subscriber('/mediapipe_gesture_recognition/right_hand', Hand, self.RightHandCallback)
+        rospy.Subscriber('/mediapipe_gesture_recognition/left_hand', Hand, self.LeftHandCallback)
+        rospy.Subscriber('/mediapipe_gesture_recognition/pose', Pose, self.PoseCallback)
 
-        self.right_msg = None
-        self.left_msg  = None
-        self.pose_msg  = None
-
+        # Variables Initialization
+        self.right_msg, self.left_msg, self.pose_msg  = Hand(), Hand(), Pose()
         self.z = -1
 
+        # Input Request
+        self.name_label = input('Insert the name of the object/area you want to register the coordinates: ')
+
     # Callback Functions
-    def RightHandCallback(self, data): self.right_msg: Hand() = data
-    def LeftHandCallback(self, data):  self.left_msg:  Hand() = data
-    def PoseCallback(self, data):      self.pose_msg:  Pose() = data
+    def RightHandCallback(self, data:Hand): self.right_msg: Hand = data
+    def LeftHandCallback(self, data:Hand):  self.left_msg:  Hand = data
+    def PoseCallback(self, data:Pose):      self.pose_msg:  Pose = data
 
-    def takeCoordinates(self):
+    def countdown(self, num_of_secs:int):
 
-        # Definizione delle coordinate
+        """ Countdown Function """
 
+        print('\nAcquisition Starts in:')
+
+        # Wait Until 0 Seconds Remaining
+        while (not rospy.is_shutdown() and num_of_secs != 0):
+
+            # Print the Remaining Time
+            print('{:02d}:{:02d}'.format(*divmod(num_of_secs, 60)))
+
+            # Wait 1 Second
+            rospy.sleep(1)
+            num_of_secs -= 1
+
+        print('\nSTART\n')
+
+    def get_coordinates(self) -> List[float]:
+
+        """ Get the Coordinates of the Point-At Gesture """
+
+        # Coordinate Definition
         x1 = float(self.pose_msg.keypoints[15].x)
         y1 = float(self.pose_msg.keypoints[15].y)
         z1 = float(self.pose_msg.keypoints[15].depth)
@@ -39,83 +60,98 @@ class ItemRegister:
         y2 = float(self.pose_msg.keypoints[13].y)
         z2 = float(self.pose_msg.keypoints[13].depth)
 
-        # Definizione dei punti
+        # Point Definition
         p1 = np.array([x1, y1, z1])
         p2 = np.array([x2, y2, z2])
 
-        # Find the point
+        # Find the 3rd Point
         p3 = self.find_point_on_line(p1, p2)
 
         return p3
 
-    def find_line_equation(self, p1, p2):
+    def find_line_equation(self, p1:List[float], p2:List[float]) -> Tuple[float]:
 
-        # Trova l'equazione della retta che passa per i punti p1 e p2
+        """ Find the Equation of the Line Passing Through p1 and p2 """
+
+        # Unpack the Coordinates        
         x1, y1, z1 = p1
         x2, y2, z2 = p2
-        dx = x2 - x1
-        dy = y2 - y1
-        dz = z2 - z1
+
+        # Calculate the Differences
+        dx, dy, dz = x2 - x1, y2 - y1, z2 - z1
+
+        # Equation of the Line: z = m * sqrt((x2 - x1)^2 + (y2 - y1)^2) + q
         m = dz / ((dx ** 2 + dy ** 2 + dz ** 2) ** 0.5)
         q = z1 - m * ((dx ** 2 + dy ** 2) ** 0.5)
 
         return m, q
 
-    def find_point_on_line(self, p1, p2):
+    def find_point_on_line(self, p1:List[float], p2:List[float]) -> List[float]:
 
-        #Trova il punto sulla retta che passa per i punti p1 e p2 con la coordinata z data
+        """ Find the Point on the Line Passing Through p1 and p2 with the Given z Coordinate """
 
+        # Find the Equation of the Line
         m, q = self.find_line_equation(p1, p2)
 
-        x1, y1, z1 = p1
+        # Unpack the Coordinates
+        x1, y1, _ = p1
 
+        # Calculate the x and y Coordinates
         x = ((self.z - q) / m) * ((p2[0] - p1[0]) / ((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)**0.5) + x1
         y = ((self.z - q) / m) * ((p2[1] - p1[1]) / ((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)**0.5) + y1
 
-        p3 = [x, y, self.z]
+        # Point Definition p3 = [x, y, z]
+        return [x, y, self.z]
 
-        return p3
+    def save_coordinates(self, label:str, p3:List[float]):
 
-    def saveCoordinates(self, label, p3):
+        """ Save the Coordinates of the Point-At Gesture """
 
+        # Initialize
         coordinates = []
         item_found = False
 
-        with open('/home/alberto/catkin_ws/src/mediapipe_gesture_recognition/doc/item_coordinates.csv', 'r') as csv_file:
+        # Read the CSV File
+        with open('item_coordinates.csv', 'r') as csv_file:
             reader = csv.reader(csv_file, delimiter=',')
 
+            # Loop Over the Rows
             for row in reader:
 
+                # Check if the Label is Already in the CSV File
                 if row[0] == label:
 
+                    # Update the Coordinates
                     row[1:4] = [str(p3[0]), str(p3[1]), str(p3[2])]
                     coordinates.append(row)
                     print("The {} are updated in x:{}, y:{}, z:{}".format(label, row[1], row[2], row[3]))
 
                     item_found = True
-                else:
-                    coordinates.append(row)
+
+                # Append the Row
+                else: coordinates.append(row)
 
             if not item_found:
 
+                # Append the New Row
                 new_row = [label, str(p3[0]), str(p3[1]), str(p3[2])]
                 coordinates.append(new_row)
                 print("The {} are now in x:{}, y:{}, z:{}".format(label, new_row[1], new_row[2], new_row[3]))
 
-        with open('/home/alberto/catkin_ws/src/mediapipe_gesture_recognition/doc/item_coordinates.csv', 'w', newline='') as csv_file:
+        # Write the CSV File
+        with open('item_coordinates.csv', 'w', newline='') as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
 
-            for row in coordinates:
-                writer.writerow(row)
+            # Loop Over the Rows -> Write the Rows
+            for row in coordinates: writer.writerow(row)
 
 if __name__ == '__main__':
 
-    R = ItemRegister()
+    # Coordinate Register Initialization
+    CR = CoordinateRegister()
 
-    name_label = input("Inserisci il nome dell'oggetto/area di cui vuoi registrare le coordinate: ")
+    # Countdown
+    CR.countdown(3)
 
-    Start_countdown(8)
-
-    item_coordinates = R.takeCoordinates()
-
-    R.saveCoordinates(name_label, item_coordinates)
+    # Take and Save Coordinates
+    CR.save_coordinates(CR.name_label, CR.get_coordinates)
--- scripts/raw_function/3Dpoint_node_area.py
+++ scripts/raw_function/3Dpoint_node_area.py
@@ -1,200 +1,132 @@
 #!/usr/bin/env python3
 
 import rospy, numpy as np
+from typing import List, Tuple
 from std_msgs.msg import Int32MultiArray
 from mediapipe_gesture_recognition.msg import Hand, Pose
 
-class Pointer:
+# Define Left and Right Hands
+LEFT, RIGHT = 0,1
+
+# Areas Coordinates Definition
+AREA_1 = np.array([-1, -0.5, -2])
+AREA_2 = np.array([3, 0.5, -2])
+
+class CoordinatePointer:
 
     def __init__(self):
 
-        # Inizializzazione del nodo
-        rospy.init_node('pointer_subscriber', anonymous=True)
+        # Node Initialization
+        rospy.init_node('coordinate_pointer', anonymous=True)
 
-        # Inizializzazione dei topic
-        rospy.Subscriber("/mediapipe_gesture_recognition/right_hand", Hand, self.RightHandCallback)
-        rospy.Subscriber("/mediapipe_gesture_recognition/left_hand", Hand, self.LeftHandCallback)
-        rospy.Subscriber("/mediapipe_gesture_recognition/pose", Pose, self.PoseCallback)
+        # Subscribers Initialization
+        rospy.Subscriber('/mediapipe_gesture_recognition/right_hand', Hand, self.RightHandCallback)
+        rospy.Subscriber('/mediapipe_gesture_recognition/left_hand', Hand, self.LeftHandCallback)
+        rospy.Subscriber('/mediapipe_gesture_recognition/pose', Pose, self.PoseCallback)
 
+        # Publisher Initialization
         self.area_pub = rospy.Publisher('area', Int32MultiArray, queue_size=1)
 
         # TODO: publish all the items and areas on fusion node
 
-        self.right_msg = None
-        self.left_msg  = None
-        self.pose_msg  = None
-        self.Area_threshold = 1.8
-        self.Item_threshold = 0.2
+        # Variables Initialization
+        self.right_msg, self.left_msg, self.pose_msg = Hand(), Hand(), Pose()
+        self.area_threshold, self.item_threshold = 1.8, 0.2
         self.z = -2
 
-    def find_line_equation(self, p1, p2):
+    # Callback Functions
+    def RightHandCallback(self, data:Hand): self.right_msg: Hand = data
+    def LeftHandCallback(self, data:Hand):  self.left_msg:  Hand = data
+    def PoseCallback(self, data:Pose):      self.pose_msg:  Pose = data
 
-        # Trova l'equazione della retta che passa per i punti p1 e p2
+    def find_line_equation(self, p1:List[float], p2:List[float]) -> Tuple[float]:
+
+        """ Find the Equation of the Line Passing Through p1 and p2 """
+
+        # Unpack the Coordinates        
         x1, y1, z1 = p1
         x2, y2, z2 = p2
-        dx = x2 - x1
-        dy = y2 - y1
-        dz = z2 - z1
+
+        # Calculate the Differences
+        dx, dy, dz = x2 - x1, y2 - y1, z2 - z1
+
+        # Equation of the Line: z = m * sqrt((x2 - x1)^2 + (y2 - y1)^2) + q
         m = dz / ((dx ** 2 + dy ** 2 + dz ** 2) ** 0.5)
         q = z1 - m * ((dx ** 2 + dy ** 2) ** 0.5)
 
         return m, q
 
-    def find_point_on_line(self, p1, p2):
+    def find_point_on_line(self, p1:List[float], p2:List[float]) -> List[float]:
 
-        #Trova il punto sulla retta che passa per i punti p1 e p2 con la coordinata z data
+        """ Find the Point on the Line Passing Through p1 and p2 with the Given z Coordinate """
 
+        # Find the Equation of the Line
         m, q = self.find_line_equation(p1, p2)
 
-        x1, y1, z1 = p1
+        # Unpack the Coordinates
+        x1, y1, _ = p1
 
+        # Calculate the x and y Coordinates
         x = ((self.z - q) / m) * ((p2[0] - p1[0]) / ((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)**0.5) + x1
         y = ((self.z - q) / m) * ((p2[1] - p1[1]) / ((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)**0.5) + y1
 
-        p3 = [x, y, self.z]
+        # Point Definition p3 = [x, y, z]
+        return [x, y, self.z]
 
-        return p3
+    def create_3DLine(self, hand:bool):
 
-    # Callback Functions
-    def RightHandCallback(self, data): self.right_msg: Hand() = data
-    def LeftHandCallback(self, data):  self.left_msg:  Hand() = data
-    def PoseCallback(self, data):      self.pose_msg:  Pose() = data
-
-    def create3DLine_Right(self):
-
-        # print(f"{'Nr':<3} | {self.pose_msg.keypoints[16].keypoint_name}")
-        # print(f"{'X':<3} | {self.pose_msg.keypoints[16].x:.2f}")
-        # print(f"{'Y':<3} | {self.pose_msg.keypoints[16].y:.2f}")
-        # print(f"{'Z':<3} | {self.pose_msg.keypoints[16].depth:.2f} \n")
-
-        # print(f"{'Nr':<3} | {self.pose_msg.keypoints[14].keypoint_name}")
-        # print(f"{'X':<3} | {self.pose_msg.keypoints[14].x:.2f}")
-        # print(f"{'Y':<3} | {self.pose_msg.keypoints[14].y:.2f}")
-        # print(f"{'Z':<3} | {self.pose_msg.keypoints[14].depth:.2f} \n")
-
-        # Definizione delle coordinate
-        x1 = float(self.pose_msg.keypoints[15].x)
-        y1 = float(self.pose_msg.keypoints[15].y)
-        z1 = float(self.pose_msg.keypoints[15].depth)
-        x2 = float(self.pose_msg.keypoints[13].x)
-        y2 = float(self.pose_msg.keypoints[13].y)
-        z2 = float(self.pose_msg.keypoints[13].depth)
-
-        # Definizione dei punti
-        p1 = np.array([x1, y1, z1])
-        p2 = np.array([x2, y2, z2])
-
-        p3_R = self.find_point_on_line(p1, p2)
-
-        Area1 = np.array([-1, -0.5, -2])
-        Area2 = np.array([3, 0.5, -2])
-
-        distancefromArea1 = np.linalg.norm(np.cross(p2-p1, p1 - Area1)) / np.linalg.norm(p2 - p1)
-        distancefromArea2 = np.linalg.norm(np.cross(p2-p1, p2 - Area2)) / np.linalg.norm(p2-p1)
-
-        print("\n\n\n\n\n")
-        print("Distance Area 1 destra: ", distancefromArea1)
-        print("Distance Area 2 destra : ", distancefromArea2)
-        print("P3_R coordinates: ", p3_R)
-        print("\n\n\n\n\n")
-
-        areamsg = Int32MultiArray()
-
-        #  # Stampa se il punto appartiene all'intorno della retta
-        # if distancefromArea1 <= self.Area_threshold:
-        #     print("Stai indicando l'Area 2 con la destra")
-        #     areamsg.data = [2]
-        #     self.area_pub.publish(areamsg)
-        #     return areamsg.data
-
-        if distancefromArea2 <= 2.2:
-            print("Stai indicando l'Area 1 con la destra")
-            areamsg.data = [1]
-            self.area_pub.publish(areamsg)
-            return areamsg.data
-
-    def create3DLine_Left(self):
+        """ Create the 3D Line """
 
         if not self.pose_msg == None:
 
-            # print(f"{'Nr':<3} | {self.pose_msg.keypoints[15].keypoint_name}")
-            # print(f"{'X':<3} | {self.pose_msg.keypoints[15].x:.2f}")
-            # print(f"{'Y':<3} | {self.pose_msg.keypoints[15].y:.2f}")
-            # print(f"{'Z':<3} | {self.pose_msg.keypoints[15].depth:.2f} \n")
+            # Coordinate Definition
+            x1 = float(self.pose_msg.keypoints[15].x)     if hand == RIGHT else float(self.pose_msg.keypoints[16].x)
+            y1 = float(self.pose_msg.keypoints[15].y)     if hand == RIGHT else float(self.pose_msg.keypoints[16].y)
+            z1 = float(self.pose_msg.keypoints[15].depth) if hand == RIGHT else float(self.pose_msg.keypoints[16].depth)
+            x2 = float(self.pose_msg.keypoints[13].x)     if hand == RIGHT else float(self.pose_msg.keypoints[14].x)
+            y2 = float(self.pose_msg.keypoints[13].y)     if hand == RIGHT else float(self.pose_msg.keypoints[14].y)
+            z2 = float(self.pose_msg.keypoints[13].depth) if hand == RIGHT else float(self.pose_msg.keypoints[14].depth)
 
-            # print(f"{'Nr':<3} | {self.pose_msg.keypoints[13].keypoint_name}")
-            # print(f"{'X':<3} | {self.pose_msg.keypoints[13].x:.2f}")
-            # print(f"{'Y':<3} | {self.pose_msg.keypoints[13].y:.2f}")
-            # print(f"{'Z':<3} | {self.pose_msg.keypoints[13].depth:.2f} \n")
-
-            # Definizione delle coordinate
-            x1 = float(self.pose_msg.keypoints[16].x)
-            y1 = float(self.pose_msg.keypoints[16].y)
-            z1 = float(self.pose_msg.keypoints[16].depth)
-
-            x2 = float(self.pose_msg.keypoints[14].x)
-            y2 = float(self.pose_msg.keypoints[14].y)
-            z2 = float(self.pose_msg.keypoints[14].depth)
-
-            # Definizione dei punti
+            # Point Definition
             p1 = np.array([x1, y1, z1])
             p2 = np.array([x2, y2, z2])
 
-            p3_L = self.find_point_on_line(p1, p2)
+            # Find the 3rd Point
+            p3 = self.find_point_on_line(p1, p2)
 
-            #Definiamo le coordinate di tre aree
-            Area1 = np.array([-1, -0.5, -2])
-            Area2 = np.array([3, 0.5, -2])
+            # Compute the Distance from the Areas
+            distance_from_area_1 = np.linalg.norm(np.cross(p2-p1, p1-AREA_1)) / np.linalg.norm(p2-p1)
+            distance_from_area_2 = np.linalg.norm(np.cross(p2-p1, p2-AREA_2)) / np.linalg.norm(p2-p1)
 
-            # Calcolo della distanza tra l'area puntata e le aree
-            distancefromArea1 = np.linalg.norm(np.cross(p2-p1, p1 - Area1)) / np.linalg.norm(p2 - p1)
-            distancefromArea2 = np.linalg.norm(np.cross(p2-p1, p2 - Area2)) / np.linalg.norm(p2-p1)
+            print('\n\n\n\n\n')
+            print(f'Distance Area 1 - {"Right" if hand == RIGHT else "Left"}: {distance_from_area_1}')
+            print(f'Distance Area 2 - {"Right" if hand == RIGHT else "Left"}: {distance_from_area_2}')
+            print(f'Point 3 Coordinates - {"Right" if hand == RIGHT else "Left"}: {p3}')
+            print("\n\n\n\n\n")
 
-            print("\n\n")
-            print("Distanza sinistra Area 1: ", distancefromArea1)
-            print("Distanza sinistra Area 2: ", distancefromArea2)
-            print("P3_L coordinates: ", p3_L)
-            print("\n\n")
+            # Initialize the ROS Message
+            area_msg = Int32MultiArray()
 
-            # itemmsg = Float32MultiArray()
-            areamsg = Int32MultiArray()
+            # TODO: Print if the Point Belongs to the Neighborhood of the Line
+            if distance_from_area_1 <= self.area_threshold:
+                print(f'You are Pointing Area 1 with the {"Right" if hand == RIGHT else "Left"} Hand')
+                area_msg.data = [1]
+                self.area_pub.publish(area_msg)
+                return area_msg.data
 
-            # Stampa se il punto appartiene all'intorno della retta
-            if distancefromArea1 <= self.Area_threshold:
-                print("Stai indicando l'Area 2 con la sinistra")
-                areamsg.data = [2]
-                self.area_pub.publish(areamsg)
-                return areamsg.data
-
-            # if distancefromArea2 <= self.Area_threshold:
-            #     print("Stai indicando l'Area 1 con la sinistra")
-            #     areamsg.data = [1]
-            #     self.area_pub.publish(areamsg)
-            #     return areamsg.data
-            print("\n\n")
+            if distance_from_area_2 <= self.area_threshold:
+                print(f'You are Pointing Area 2 with the {"Right" if hand == RIGHT else "Left"} Hand')
+                area_msg.data = [2]
+                self.area_pub.publish(area_msg)
+                return area_msg.data
 
 if __name__ == '__main__':
 
-    P = Pointer()
-
-    # mode = input("Quale configurazione vuoi utilizzare? Destra (R), Sinistra (L) o Entrambe (B)?")
-    mode = 'B'
+    CP = CoordinatePointer()
 
     while not rospy.is_shutdown():
 
         rospy.sleep(0.25)
 
-        if mode == "R" or mode == 'r' or mode == "right" or mode == "destra" or mode == 'D' or mode == 'd':
-
-            P.create3DLine_Right()
-
-        elif mode == "L" or mode == 'l' or mode == "left" or mode == "sinistra" or mode == 'S' or mode == 's':
-
-            P.create3DLine_Left()
-
-        elif mode == "B" or mode == 'b' or mode == "both" or mode == "entrambe" or mode == 'E' or mode == 'e':
-
-            Area_sinistra = P.create3DLine_Left()
-            Area_destra = P.create3DLine_Right()
-
-            print(Area_sinistra, Area_destra)
+        # Print and Publish the Detected Areas
+        print(CP.create_3DLine(LEFT), CP.create_3DLine(RIGHT))
